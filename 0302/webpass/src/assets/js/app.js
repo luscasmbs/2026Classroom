a30211198f79d6fd74d82c5787d52c316b67e2dd
luscasmbs <lucasmiguelbsilva@gmail.com>
2026-02-03 12:12:33 -0300

    Add files via upload

---
+++ 0302/webpass/src/assets/js/app.js
@@ -0,0 +1,10 @@
+function cadastrar() {
+            let codigo = Math.floor(Math.random() * 10000).toString().padStart(4, '0');
+            alert(codigo)
+}
+function cpf() {
+        let valor = document.getElementById('cod').value;
+}
+function seil() {
+    
+}
